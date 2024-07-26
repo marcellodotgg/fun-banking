b1837fc53ce6f0a1a089a3135c2f1816fd8fc056
Marcello <marcello@bytebury.com>
2024-07-25 20:25:09 -0400

    feature: validations for accounts

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,9 +1,33 @@
 package domain
 
+import (
+	"errors"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
 type Account struct {
 	Audit
 	Name       string   `gorm:"not null; default:Checking"`
 	Balance    float64  `gorm:"decimal(50,2); default:0.00"`
 	CustomerID int      `gorm:"not null"`
 	Customer   Customer `gorm:"foreignKey:CustomerID; constraint:OnDelete:CASCADE"`
+}
+
+func (a *Account) BeforeCreate(tx *gorm.DB) error {
+	a.Name = strings.TrimSpace(a.Name)
+	return a.validate()
+}
+
+func (a *Account) BeforeUpdate(tx *gorm.DB) error {
+	a.Name = strings.TrimSpace(a.Name)
+	return a.validate()
+}
+
+func (a Account) validate() error {
+	if len(a.Name) > 25 {
+		return errors.New("name too long")
+	}
+	return nil
 }
