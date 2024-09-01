ca595aa5cbc8cfcd16fe8723df6959652295d921
Marcello <marcello@bytebury.com>
2024-09-01 15:59:10 -0400

    feature: account notes (#17)
    * feature: add account descriptions
    
    * feature: account notes

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -9,11 +9,12 @@ import (
 
 type Account struct {
 	Audit
-	Name       string   `gorm:"not null; default:Checking"`
-	Balance    float64  `gorm:"decimal(50,2); default:0.00"`
-	CustomerID int      `gorm:"not null"`
-	Customer   Customer `gorm:"foreignKey:CustomerID; constraint:OnDelete:CASCADE"`
-	IsPrimary  bool     `gorm:"not null;default:0"`
+	Name        string   `gorm:"not null; default:Checking"`
+	Balance     float64  `gorm:"decimal(50,2); default:0.00"`
+	CustomerID  int      `gorm:"not null"`
+	Customer    Customer `gorm:"foreignKey:CustomerID; constraint:OnDelete:CASCADE"`
+	IsPrimary   bool     `gorm:"not null;default:0"`
+	Description string
 }
 
 func (a *Account) BeforeCreate(tx *gorm.DB) error {
